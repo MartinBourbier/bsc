5a08240560841d46522461728013a453a39d5fc3
martin.bourbier <martin.bourbier@epita.fr>
2025-03-06 21:06:59 +0100

    feat(bsc-parse): handle identifier parsing

--- bsc-parse/src/lib.rs
+++ bsc-parse/src/lib.rs
@@ -5,10 +5,41 @@ mod tests {
     use super::*;
 
     #[test]
-    fn grammar_test() {
-        assert!(grammar::TermParser::new().parse("22").is_ok());
-        assert!(grammar::TermParser::new().parse("(22)").is_ok());
-        assert!(grammar::TermParser::new().parse("((((22))))").is_ok());
-        assert!(grammar::TermParser::new().parse("((22)").is_err());
+    fn grammar_id_simple_test() {
+        let sym = grammar::IdParser::new().parse("a");
+
+        assert!(sym.is_ok());
+        assert_eq!(sym.unwrap().get_name(), "a");
+    }
+
+    #[test]
+    fn grammar_id_underscore_prefix_test() {
+        let sym = grammar::IdParser::new().parse("_a");
+
+        assert!(sym.is_ok());
+        assert_eq!(sym.unwrap().get_name(), "_a");
+    }
+
+    #[test]
+    fn grammar_id_wrong_prefix_test() {
+        let sym = grammar::IdParser::new().parse("1a");
+
+        assert!(sym.is_err());
+    }
+
+    #[test]
+    fn grammar_id_alphanumeric_test() {
+        let sym = grammar::IdParser::new().parse("a1");
+
+        assert!(sym.is_ok());
+        assert_eq!(sym.unwrap().get_name(), "a1");
+    }
+
+    #[test]
+    fn grammar_id_alphanumeric_underscore_test() {
+        let sym = grammar::IdParser::new().parse("a_1");
+
+        assert!(sym.is_ok());
+        assert_eq!(sym.unwrap().get_name(), "a_1");
     }
 }
