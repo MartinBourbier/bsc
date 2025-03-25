928c7d36e134a41a4a3f02974b268b7199bdb55c
Martin Bourbier <martin.bourbier@epita.fr>
2025-03-25 20:42:05 +0100

    feat(bsc-lexer): many more stuff

--- bsc-lexer/src/lib.rs
+++ bsc-lexer/src/lib.rs
@@ -1,8 +1,17 @@
 use logos::{Lexer, Logos};
+use std::fmt;
+use std::num::ParseIntError;
+
+macro_rules! lowercase {
+    ($i:ident) => {
+        format!("{:?}", $i).to_lowercase()
+    };
+}
 
 #[derive(Default, Debug, Clone, PartialEq)]
-enum LexingError {
+pub enum LexingError {
     ForbiddenIdentifier(String),
+    InvalidInteger(String),
     #[default]
     InvalidToken,
 }
@@ -11,26 +20,48 @@ fn forbidden_identifier(lex: &mut Lexer<CToken>) -> Result<(), LexingError> {
     Err(LexingError::ForbiddenIdentifier(lex.slice().to_string()))
 }
 
-enum ConstantValue {
-    IntegerConstant(/* help */),
-    StringConstant(String),
+impl From<ParseIntError> for LexingError {
+    fn from(err: ParseIntError) -> Self {
+        use std::num::IntErrorKind::*;
+        match err.kind() {
+            PosOverflow | NegOverflow => LexingError::InvalidInteger("overflow error".to_owned()),
+            _ => LexingError::InvalidInteger("other error".to_owned()),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum Punctuator {
+    LBRACK, // [
+    RBRACK, // ]
+    LPAREN, // (
+    RPAREN, // )
+    LBRACE, // {
+    RBRACE, // }
+    DOT, // .
+    LARROW, // ->
+    DPLUS, // ++
+    DMINUS, // --
+    AMP, // &
+    STAR, // *
+    PLUS, // +
+    MINUS, // -
+    TILDE, // ~
+    BANG, // !
+    // TODO: add the remaining ones, section 6.4.6 of ISO/IEC 9899
+}
+
+fn make_punctuator(punctuator: Punctuator) -> impl Fn(&mut Lexer<CToken>) -> Punctuator {
+    move |_| punctuator
 }
 
 #[derive(Logos, Debug, PartialEq)]
 #[logos(error = LexingError)]
 #[logos(skip r"[ \t\n\f]+")] // Ignore this regex pattern between tokens
+#[logos(skip r"// [^\n]*")] // Single line comments
 // Common subpatterns
-#[logos(subpattern alpha = r"[a-zA-Z]")]
-#[logos(subpattern digit = r"[0-9]")]
-#[logos(subpattern alphanum = r"(?&alpha)|(?&digit)")]
-#[logos(subpattern wordchr = r"(?&alphanum)|(_)")]
-#[logos(subpattern identifier = r"[a-zA-Z_](?&wordchr)*")]
-// Constant subpatterns
-#[logos(subpattern us = r"(u)|(U)")] // unsigned suffix
-#[logos(subpattern ls = r"(l)|(L)")] // long suffix
-#[logos(subpattern lls = r"(ll)|(LL)")] // long long suffix
-#[logos(subpatterns interger-suffix = r"((?&us)(ls)?)|((?&us)(?&lls))|((?&ls)(?&us)?)|((?&lls)(?&us)?)")]
-enum CToken {
+#[logos(subpattern identifier = r"[a-zA-Z_][[:word:]]*")]
+pub enum CToken {
     // Keyword tokens
     #[token("auto")]
     Auto,
@@ -107,8 +138,39 @@ enum CToken {
     FuncIdentifier,
 
     // TODO: handle universal character names, as stated in section 6.4.3.1 of the ISO/IEC 9899
-    #[regex("(?&identifier)")]
-    Identifier,
+    #[regex("[a-zA-Z_][[:word:]]*", |lex| lex.slice().to_string())]
+    Identifier(String),
+
+    // TODO: handle oct, hex and suffixes, section 6.4.4.1 of the ISO/IEC 9899
+    #[regex("[[:digit:]]*", |lex| lex.slice().parse::<i32>())]
+    IntegerConstant(i32),
+
+    // TODO: handle floating constants
+    // TODO: handle character constants
+    // TODO: handle string literals
+
+    // TODO: handle the other ones
+    #[token("[", callback = make_punctuator(Punctuator::LBRACK))]
+    #[token("<:", callback = make_punctuator(Punctuator::LBRACK))] // digraph
+    #[token("]", callback = make_punctuator(Punctuator::RBRACK))]
+    #[token(":>", callback = make_punctuator(Punctuator::RBRACK))] // digraph
+    #[token("(", callback = make_punctuator(Punctuator::LPAREN))]
+    #[token(")", callback = make_punctuator(Punctuator::RPAREN))]
+    #[token("{", callback = make_punctuator(Punctuator::LBRACE))]
+    #[token("<%", callback = make_punctuator(Punctuator::LBRACE))] // digraph
+    #[token("}", callback = make_punctuator(Punctuator::RBRACE))]
+    #[token("%>", callback = make_punctuator(Punctuator::RBRACE))] // digraph
+    Punctuator(Punctuator)
+}
+
+impl fmt::Display for CToken {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Identifier(id) => write!(f, "id: {}", id),
+            Self::IntegerConstant(n) => write!(f, "int: {}", n),
+            _ => write!(f, "{}", lowercase!(self)),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -135,21 +197,17 @@ mod tests {
     fn test_simple_identifiers() {
         let mut lex = CToken::lexer("a b_ c1234_ amazing_func0 _a");
 
-        assert_eq!(lex.next(), Some(Ok(CToken::Identifier)));
-        assert_eq!(lex.slice(), "a");
-
-        assert_eq!(lex.next(), Some(Ok(CToken::Identifier)));
-        assert_eq!(lex.slice(), "b_");
-
-        assert_eq!(lex.next(), Some(Ok(CToken::Identifier)));
-        assert_eq!(lex.slice(), "c1234_");
-
-        assert_eq!(lex.next(), Some(Ok(CToken::Identifier)));
-        assert_eq!(lex.slice(), "amazing_func0");
-
-        assert_eq!(lex.next(), Some(Ok(CToken::Identifier)));
-        assert_eq!(lex.slice(), "_a");
-
+        assert_eq!(lex.next(), Some(Ok(CToken::Identifier("a".to_string()))));
+        assert_eq!(lex.next(), Some(Ok(CToken::Identifier("b_".to_string()))));
+        assert_eq!(
+            lex.next(),
+            Some(Ok(CToken::Identifier("c1234_".to_string())))
+        );
+        assert_eq!(
+            lex.next(),
+            Some(Ok(CToken::Identifier("amazing_func0".to_string())))
+        );
+        assert_eq!(lex.next(), Some(Ok(CToken::Identifier("_a".to_string()))));
         assert_eq!(lex.next(), None);
     }
 
@@ -178,8 +236,61 @@ mod tests {
             )))
         );
 
-        assert_eq!(lex.next(), Some(Ok(CToken::Identifier)));
-        assert_eq!(lex.slice(), "main");
+        assert_eq!(lex.next(), Some(Ok(CToken::Identifier("main".to_string()))));
+
+        assert_eq!(lex.next(), None);
+    }
+
+    #[test]
+    fn test_integer_constant() {
+        let mut lex = CToken::lexer("42 21");
+
+        assert_eq!(lex.next(), Some(Ok(CToken::IntegerConstant(42))));
+        assert_eq!(lex.next(), Some(Ok(CToken::IntegerConstant(21))));
+
+        assert_eq!(lex.next(), None);
+    }
+
+    #[test]
+    fn test_punctuators_brackets() {
+        let mut lex = CToken::lexer("[ <: :> ]");
+
+        assert_eq!(lex.next(), Some(Ok(CToken::Punctuator(Punctuator::LBRACK))));
+        assert_eq!(lex.next(), Some(Ok(CToken::Punctuator(Punctuator::LBRACK))));
+        assert_eq!(lex.next(), Some(Ok(CToken::Punctuator(Punctuator::RBRACK))));
+        assert_eq!(lex.next(), Some(Ok(CToken::Punctuator(Punctuator::RBRACK))));
+
+        assert_eq!(lex.next(), None);
+    }
+
+    #[test]
+    fn test_punctuators_braces() {
+        let mut lex = CToken::lexer("{ <% %> }");
+
+        assert_eq!(lex.next(), Some(Ok(CToken::Punctuator(Punctuator::LBRACE))));
+        assert_eq!(lex.next(), Some(Ok(CToken::Punctuator(Punctuator::LBRACE))));
+        assert_eq!(lex.next(), Some(Ok(CToken::Punctuator(Punctuator::RBRACE))));
+        assert_eq!(lex.next(), Some(Ok(CToken::Punctuator(Punctuator::RBRACE))));
+
+        assert_eq!(lex.next(), None);
+    }
+
+    #[test]
+    fn test_single_line_comment_skip() {
+        let mut lex = CToken::lexer("// toto tata titi\nmain");
+
+        assert_eq!(lex.next(), Some(Ok(CToken::Identifier("main".to_string()))));
+
+        assert_eq!(lex.next(), None);
+    }
+
+    #[test]
+    fn test_single_line_comment_skip_mid_line() {
+        let mut lex = CToken::lexer("some ids // toto tata titi\nmain");
+
+        assert_eq!(lex.next(), Some(Ok(CToken::Identifier("some".to_string()))));
+        assert_eq!(lex.next(), Some(Ok(CToken::Identifier("ids".to_string()))));
+        assert_eq!(lex.next(), Some(Ok(CToken::Identifier("main".to_string()))));
 
         assert_eq!(lex.next(), None);
     }
