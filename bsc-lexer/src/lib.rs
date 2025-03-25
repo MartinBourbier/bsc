bbfcc2490ea0293675e915a50ecf8ab4889a0d2b
Martin Bourbier <martin.bourbier@epita.fr>
2025-03-25 20:41:49 +0100

    feat(bsc-lexer): move keyword lexer in ctoken lexer

--- bsc-lexer/src/lib.rs
+++ bsc-lexer/src/lib.rs
@@ -2,113 +2,77 @@ use logos::Logos;
 
 #[derive(Logos, Debug, PartialEq)]
 #[logos(skip r"[ \t\n\f]+")] // Ignore this regex pattern between tokens
-enum Keyword {
+enum CToken {
+    // Keyword tokens
     #[token("auto")]
     Auto,
-
     #[token("break")]
     Break,
-
     #[token("case")]
     Case,
-
     #[token("char")]
     Char,
-
     #[token("const")]
     Const,
-
     #[token("continue")]
     Continue,
-
     #[token("default")]
     Default,
-
     #[token("do")]
     Do,
-
     #[token("double")]
     Double,
-
     #[token("else")]
     Else,
-
     #[token("enum")]
     Enum,
-
     #[token("extern")]
     Extern,
-
     #[token("float")]
     Float,
-
     #[token("for")]
     For,
-
     #[token("goto")]
     Goto,
-
     #[token("if")]
     If,
-
     #[token("inline")]
     Inline,
-
     #[token("int")]
     Int,
-
     #[token("long")]
     Long,
-
     #[token("register")]
     Register,
-
     #[token("restrict")]
     Restrict,
-
     #[token("return")]
     Return,
-
     #[token("short")]
     Short,
-
     #[token("signed")]
     Signed,
-
     #[token("sizeof")]
     Sizeof,
-
     #[token("static")]
     Static,
-
     #[token("struct")]
     Struct,
-
     #[token("switch")]
     Switch,
-
     #[token("typedef")]
     Typedef,
-
     #[token("union")]
     Union,
-
     #[token("unsigned")]
     Unsigned,
-
     #[token("void")]
     Void,
-
     #[token("volatile")]
     Volatile,
-
     #[token("while")]
     While,
-}
 
-#[derive(Logos, Debug, PartialEq)]
-#[logos(skip r"[ \t\n\f]+")] // Ignore this regex pattern between tokens
-enum CToken {
     // TODO: make this produce an error. __func__ is reserved, as stated in section 6.4.2.2.1 of
     // the ISO/IEC 9899
     #[token("__func__")]
@@ -125,15 +89,15 @@ mod tests {
 
     #[test]
     fn test_random_default_keywords() {
-        let mut lex = Keyword::lexer("void volatile struct");
+        let mut lex = CToken::lexer("void volatile struct");
 
-        assert_eq!(lex.next(), Some(Ok(Keyword::Void)));
+        assert_eq!(lex.next(), Some(Ok(CToken::Void)));
         assert_eq!(lex.slice(), "void");
 
-        assert_eq!(lex.next(), Some(Ok(Keyword::Volatile)));
+        assert_eq!(lex.next(), Some(Ok(CToken::Volatile)));
         assert_eq!(lex.slice(), "volatile");
 
-        assert_eq!(lex.next(), Some(Ok(Keyword::Struct)));
+        assert_eq!(lex.next(), Some(Ok(CToken::Struct)));
         assert_eq!(lex.slice(), "struct");
 
         assert_eq!(lex.next(), None);
