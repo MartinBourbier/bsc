169e713ba0fe9253cefc62e61682e0f3ac9903a5
Martin Bourbier <martin.bourbier@epita.fr>
2025-04-15 14:14:48 +0200

    feat(bsc-lexer): adapt interface to work better with lalrpop

--- bsc/src/main.rs
+++ bsc/src/main.rs
@@ -1,17 +1,10 @@
-use bsc_lexer::{Lexer, dummy::DummyLexer, logos::LogosLexer};
+use bsc_lexer::{Lexer, logos::LogosLexer};
+use bsc_parse::grammar;
 
 fn main() {
-    let input = "123 456 789";
-    let dummy_lexer = DummyLexer::new(input);
+    let res = grammar::ProgramParser::new()
+        .parse(LogosLexer::new("test toto tata"))
+        .unwrap();
 
-    for tok in dummy_lexer {
-        println!("{:?}", tok);
-    }
-
-    let input = r#"int main(void) { printf(u8"Hello," " World!\n"); return 42; }"#;
-    let logos_lexer = LogosLexer::new(input);
-
-    for tok in logos_lexer {
-        println!("{:?}", tok);
-    }
+    println!("{:?}", res);
 }
