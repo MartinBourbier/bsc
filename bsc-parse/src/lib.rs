9664266ba222b2a5f92221e072ec076825ba37a9
Martin Bourbier <martin.bourbier@epita.fr>
2025-04-15 14:32:19 +0200

    feat(bsc-parse): make lalrpop use a custom lexer

--- bsc-parse/src/lib.rs
+++ bsc-parse/src/lib.rs
@@ -1,14 +1,1 @@
-lalrpop_util::lalrpop_mod!(grammar);
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn grammar_test() {
-        assert!(grammar::TermParser::new().parse("22").is_ok());
-        assert!(grammar::TermParser::new().parse("(22)").is_ok());
-        assert!(grammar::TermParser::new().parse("((((22))))").is_ok());
-        assert!(grammar::TermParser::new().parse("((22)").is_err());
-    }
-}
+lalrpop_util::lalrpop_mod!(pub grammar);
