849397b377f75dc6fa16fb2cbd9eeeb69ce72b8b
Martin Bourbier <martin.bourbier@epita.fr>
2025-04-16 11:38:04 +0200

    wip

--- bsc/src/main.rs
+++ bsc/src/main.rs
@@ -2,9 +2,27 @@ use bsc_lexer::{Lexer, logos::LogosLexer};
 use bsc_parse::grammar;
 
 fn main() {
-    let res = grammar::ProgramParser::new()
-        .parse(LogosLexer::new("test toto tata"))
+    // let mut errors = Vec::new();
+    // let res = grammar::TranslationUnitParser::new()
+    //     .parse(
+    //         &mut errors,
+    //         LogosLexer::new(r#"(_Generic (toto, default : test))"#),
+    //     )
+    //     .unwrap();
+
+    // println!("{:?}", errors);
+    // println!("{}", res);
+    // println!("{:?}", res);
+
+    let mut errors = Vec::new();
+    let res = grammar::PointerParser::new()
+        .parse(
+            &mut errors,
+            LogosLexer::new(r#"*"#),
+        )
         .unwrap();
 
+    println!("{:?}", errors);
+    println!("{}", res);
     println!("{:?}", res);
 }
